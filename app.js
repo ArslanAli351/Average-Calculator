b36a0f064d3553159da3e9af5d5e7175d1910f3a
Arslan Ali <muhammadarslanrajput12@gmail.com>
2024-09-24 09:03:33 +0500

    Average-Calculator

---
+++ app.js
@@ -0,0 +1,33 @@
+// Example
+
+// let a=1
+// a+=5
+// console.log(a,"a");
+
+
+
+
+
+const average= (array)=>{
+    let num=0
+for (let i = 0; i < array.length; i++) {
+num+= array[i];
+}
+const result = num/array.length;
+console.log(result);
+
+}
+
+average([2,3])
+
+
+
+
+    
+
+
+
+
+
+
+
